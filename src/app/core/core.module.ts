ddfe25fad2c8bec489ebcc88c6ec1af1c22e981d
JianhangLin <linjianhang1010@gmail.com>
2020-03-24 23:52:50 +0800

    feat(core): load core only one time

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
@@ -9,4 +9,12 @@ import { CommonModule } from '@angular/common';
     CommonModule
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+
+  // load core only one time
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+    if (parent) {
+      throw new Error('Core模块已经存在，不能再次加载!');
+    }
+  }
+}
