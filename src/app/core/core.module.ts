a01c510ca272bfb6e93c02e17e158360e3fe9bb0
JianhangLin <linjianhang1010@gmail.com>
2020-03-25 00:01:50 +0800

    feat(core): add header component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,11 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
     CommonModule
   ]
