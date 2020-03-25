df84f12469143f343539a5f75b84ea76e934da51
JianhangLin <linjianhang1010@gmail.com>
2020-03-25 09:05:43 +0800

    feat(core): replace CommonModule with SharedModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,15 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, SidebarComponent],
   imports: [
-    CommonModule
+    SharedModule
   ],
   exports: [
     HeaderComponent,
