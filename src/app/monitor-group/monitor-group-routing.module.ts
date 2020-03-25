728b9ee9ebcbcdd63e76fcbfda591f4ccb16745e
JianhangLin <linjianhang1010@gmail.com>
2020-03-25 12:19:40 +0800

    feat(monitor-group): define monitor group list path router

--- src/app/monitor-group/monitor-group-routing.module.ts
+++ src/app/monitor-group/monitor-group-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MonitorGroupListComponent } from './monitor-group-list/monitor-group-list.component';
 
 
 const routes: Routes = [
-
+  {path: 'monitor_groups', component: MonitorGroupListComponent}
 ];
 
 @NgModule({
--- src/app/monitor-group/monitor-group.module.ts
+++ src/app/monitor-group/monitor-group.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { MonitorGroupRoutingModule } from './monitor-group-routing.module';
 import { MonitorGroupListComponent } from './monitor-group-list/monitor-group-list.component';
-
 
 @NgModule({
   declarations: [MonitorGroupListComponent],
   imports: [
-    SharedModule
+    SharedModule,
+    MonitorGroupRoutingModule
   ]
 })
 export class MonitorGroupModule { }
