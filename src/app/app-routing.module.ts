4d0dcf06819f5b8503d2dd54a856c01bff8163fe
JianhangLin <linjianhang1010@gmail.com>
2020-03-25 12:54:19 +0800

    feat(app): define default empty path page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/monitor_groups', pathMatch: 'full'}
+  {path: '', component: AppComponent},
+  {path: 'monitor_groups', redirectTo: '/monitor_groups', pathMatch: 'full'}
 ];
 
 @NgModule({
