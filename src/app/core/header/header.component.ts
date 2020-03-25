e03c441879173e4531d1e8d1d671c17784dc63c2
JianhangLin <linjianhang1010@gmail.com>
2020-03-25 09:32:58 +0800

    feat(app): implement openSideBar event

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() sidebarToggel = new EventEmitter<void>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   openSideBar() {
-
+    this.sidebarToggel.emit();
   }
 }
