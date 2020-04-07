fef8c8cfd36ff0522ed3980478237ea32ad49518
JianhangLin <linjianhang1010@gmail.com>
2020-04-07 16:55:27 +0800

    feat(home): add step item component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { HomeListComponent } from './home-list/home-list.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { StepItemComponent } from './step-item/step-item.component';
 
 
 
 @NgModule({
-  declarations: [HomeListComponent],
+  declarations: [HomeListComponent, StepItemComponent],
   imports: [
     SharedModule,
     HomeRoutingModule
---
+++ src/app/home/step-item/step-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-item',
+  templateUrl: './step-item.component.html',
+  styleUrls: ['./step-item.component.scss']
+})
+export class StepItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
