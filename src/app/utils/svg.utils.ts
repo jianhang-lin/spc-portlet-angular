2f39141122f53b876d498551feb1d3d8cfaa0594
JianhangLin <linjianhang1010@gmail.com>
2020-04-08 09:20:07 +0800

    feat(utils): add settings_applications icon

--- src/app/utils/svg.utils.ts
+++ src/app/utils/svg.utils.ts
@@ -4,5 +4,6 @@ import { DomSanitizer } from '@angular/platform-browser';
 export const loadSvgResouces = (mir: MatIconRegistry, ds: DomSanitizer) => {
   const imgDir = 'assets/img';
   const iconDir = `${imgDir}/icons`;
+  mir.addSvgIcon('settings_applications', ds.bypassSecurityTrustResourceUrl(`${iconDir}/settings_applications-24px.svg`));
   mir.addSvgIcon('add', ds.bypassSecurityTrustResourceUrl(`${iconDir}/add.svg`));
 };
