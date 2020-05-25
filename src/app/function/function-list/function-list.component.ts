b5af9cd041a6ee5a2d7a155dbc000fd267d96bdd
JianhangLin <linjianhang1010@gmail.com>
2020-05-25 15:38:30 +0800

    feat(function): add function list component

---
+++ src/app/function/function-list/function-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-function-list',
+  templateUrl: './function-list.component.html',
+  styleUrls: ['./function-list.component.scss']
+})
+export class FunctionListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/function/function.module.ts
+++ src/app/function/function.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FunctionListComponent } from './function-list/function-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FunctionListComponent],
   imports: [
     CommonModule
   ]
