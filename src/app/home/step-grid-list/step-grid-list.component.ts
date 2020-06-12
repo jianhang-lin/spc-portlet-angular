9c0e0c348e408f142788fae511bb93107f81615a
JianhangLin <linjianhang1010@gmail.com>
2020-06-12 15:01:55 +0800

    feat(home): implement step grid list component

--- src/app/home/step-grid-list/step-grid-list.component.ts
+++ src/app/home/step-grid-list/step-grid-list.component.ts
@@ -1,4 +1,11 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import * as stepGridAction from '../../actions/step-grid.action';
+import * as fromRoot from '../../reducers';
+import { StepGridModel } from '../../domain/step-grid.model';
+
 
 @Component({
   selector: 'app-step-grid-list',
@@ -8,12 +15,29 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class StepGridListComponent implements OnInit {
 
   @Output() monitorGroupClick = new EventEmitter<void>();
-  constructor() { }
+  stepGrids$: Observable<StepGridModel[]>;
+  listAnim$: Observable<number>;
+  constructor(
+    private store$: Store
+  ) {
+    this.store$.dispatch(new stepGridAction.LoadStepGridsAction(null));
+    this.stepGrids$ = this.store$.select(fromRoot.getStepGrids);
+    this.listAnim$ = this.stepGrids$.pipe(map(s => s.length));
+  }
 
   ngOnInit(): void {
   }
 
   onMonitorGroupClick() {
     this.monitorGroupClick.emit();
+  }
+
+  selectStepGrid(index: number) {
+    console.log(index);
+    if (index === 5) {
+      this.store$.dispatch(new stepGridAction.SelectEnterAction(index));
+    } else {
+      this.store$.dispatch(new stepGridAction.SelectEnterFailAction(null));
+    }
   }
 }
