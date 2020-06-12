5e30ccc4a0f6eb62dde01f634362e92dd84b868d
JianhangLin <linjianhang1010@gmail.com>
2020-06-12 14:59:52 +0800

    feat(effects): add router effects

--- src/app/effects/index.ts
+++ src/app/effects/index.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
+import { RouterEffects } from './router.effects';
+import { StepGridsEffects } from './step-grids.effects';
 import { MonitorGroupEffects } from './monitor-group.effects';
 import { MonitorEffects } from './monitor.effects';
 import { MonitorDetailsEffects } from './monitor-details.effects';
@@ -7,6 +9,8 @@ import { MonitorDetailsEffects } from './monitor-details.effects';
 @NgModule({
   imports: [
     EffectsModule.forRoot([
+      RouterEffects,
+      StepGridsEffects,
       MonitorGroupEffects,
       MonitorEffects,
       MonitorDetailsEffects,
