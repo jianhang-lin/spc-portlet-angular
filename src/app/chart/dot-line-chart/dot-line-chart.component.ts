cf9ccfc6a2940d8fe85775b166fbba1883541381
JianhangLin <linjianhang1010@gmail.com>
2020-08-18 16:59:16 +0800

    feat(chart): implement multi lines with d3js

--- src/app/chart/dot-line-chart/dot-line-chart.component.ts
+++ src/app/chart/dot-line-chart/dot-line-chart.component.ts
@@ -2,8 +2,11 @@ import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChi
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 import * as d3 from 'd3';
-
+import * as fromReducers from '../../reducers';
+import * as dotLineChartAction from '../../actions/dot-line-chart.action';
+import { DotLineDataModel } from '../../domain/dot-line-data.model';
 
 @Component({
   selector: 'app-dot-line-chart',
@@ -11,17 +14,12 @@ import * as d3 from 'd3';
   styleUrls: ['./dot-line-chart.component.scss']
 })
 export class DotLineChartComponent implements OnInit, OnChanges {
-  constructor(public router: Router, private route: ActivatedRoute, private http: HttpClient) {
-     this.width = 1200; // window.innerWidth - this.margin.left - this.margin.right;
-     this.height = 400; // window.innerHeight - this.margin.top - this.margin.bottom;
-  }
-
   public title = 'Line Chart';
   public data: any;
   private margin = {top: 20, right: 20, bottom: 30, left: 30};
   public width: number;
   private height: number;
-  private x: d3.ScaleTime<number, number>;
+  private x: d3.ScaleOrdinal<string, any>;
   private y: d3.ScaleLinear<number, number>;
   private svg: d3.Selection<any, any, HTMLElement, any>;
   private line: d3.Line<[number, number]>;
@@ -35,15 +33,72 @@ export class DotLineChartComponent implements OnInit, OnChanges {
   private colors: any;
   private xAxis: d3.Axis<d3.AxisDomain>;
   private yAxis: d3.Axis<d3.AxisDomain>;
+  private xRangeArray: any;
+
+  dotLineDataList: DotLineDataModel[];
+  dotLineDataList$: Observable<DotLineDataModel[]>;
+  constructor(public router: Router,
+              private route: ActivatedRoute,
+              private http: HttpClient,
+              private store$: Store<fromReducers.State>) {
+    this.width = 1200; // window.innerWidth - this.margin.left - this.margin.right;
+    this.height = 400; // window.innerHeight - this.margin.top - this.margin.bottom;
+    this.store$.dispatch(new dotLineChartAction.LoadDotLineDataAction(null));
+    this.dotLineDataList$ = this.store$.select(fromReducers.getDotLineDataList);
+  }
 
   ngOnInit(): void {
-    this.getData().subscribe(data => {
-
-      this.dataProcess(data);
-      this.buildSvg();
-      this.addAxis();
-      this.drawLineAndPath();
-      this.showTip();
+    this.dotLineDataList$.subscribe(dotLineDataList => {
+      if (dotLineDataList.length > 0) {
+        this.dotLineDataList = dotLineDataList;
+        const parseTime = d3.timeParse('%Y-%m-%d %H:%M:%S');
+        const labelDateTimes = [];
+        const data1 = [];
+        const ucl1 = [];
+        const lcl1 = [];
+        const target1 = [];
+        const data2 = [];
+        const ucl2 = [];
+        const lcl2 = [];
+        const target2 = [];
+        this.dotLineDataList.forEach( (dotlineData, index) => {
+          if (index % 4 === 0) {
+            labelDateTimes.push(dotlineData.labelDateTimeStr);
+          }
+        });
+        this.dotLineDataList.forEach((dotlineData, index) => {
+          if (dotlineData.dotDimensionName === 'data1' && !dotlineData.hideRow) {
+            data1.push(dotlineData.data);
+            ucl1.push(dotlineData.ucl);
+            lcl1.push(dotlineData.lcl);
+            target1.push(dotlineData.target);
+          }
+          if (dotlineData.dotDimensionName === 'data2' && !dotlineData.hideRow) {
+            data2.push(dotlineData.data);
+            ucl2.push(dotlineData.ucl);
+            lcl2.push(dotlineData.lcl);
+            target2.push(dotlineData.target);
+          }
+        });
+        this.data = Object.assign({
+          y: '% Unemployment',
+          series: [
+            {name: 'data1', values: data1},
+            {name: 'ucl1', values: ucl1},
+            {name: 'lcl1', values: lcl1},
+            {name: 'target1', values: target1},
+            {name: 'data2', values: data2},
+            {name: 'ucl2', values: ucl2},
+            {name: 'lcl2', values: lcl2},
+            {name: 'target2', values: target2},
+          ],
+          dates: labelDateTimes,
+        });
+        this.buildSvg();
+        this.addAxis();
+        this.drawLineAndPath();
+        this.showTip();
+      }
     });
 
     // this.createChart();
@@ -61,7 +116,7 @@ export class DotLineChartComponent implements OnInit, OnChanges {
   private dataProcess(data: string): void {
     const tsvArray = d3.tsvParse(data);
     const columns = tsvArray.columns.slice(1);
-    this.data = Object.assign({
+    const t = Object.assign({
       y: '% Unemployment',
       series: tsvArray.map(d => ({
         name: d.name.replace(/, ([\w-]+).*/, ' $1'),
@@ -78,13 +133,20 @@ export class DotLineChartComponent implements OnInit, OnChanges {
   }
 
   private addAxis(): void {
-    this.x = d3.scaleUtc().range([this.margin.left, this.width - this.margin.right]);
-    this.y = d3.scaleLinear()
-      .range([this.height - this.margin.bottom, this.margin.top]);
-    this.x.domain(d3.extent(this.data.dates as Date[]));
-    this.y.domain([0, d3.max(this.data.series, (d: any) => d3.max(d.values) as unknown as number)]).nice();
+    const xAxisWidth = this.width - this.margin.right - this.margin.left;
+    const xAxisTicksCount = this.data.series[0].values.length;
+    const singleXAxisWidth = xAxisWidth / xAxisTicksCount;
+    this.xRangeArray = [];
+    for (let i = 0; i < xAxisTicksCount; i++) {
+      this.xRangeArray.push(this.margin.left + singleXAxisWidth * i);
+    }
+    this.x = d3.scaleOrdinal().range(this.xRangeArray);
+    this.y = d3.scaleLinear().range([this.height - this.margin.bottom, this.margin.top]);
+    this.x.domain(this.data.dates);
+    this.y.domain([d3.min(this.data.series, (d: any) => d3.min(d.values) as unknown as number),
+      d3.max(this.data.series, (d: any) => d3.max(d.values) as unknown as number)]).nice();
 
-    this.xAxis = d3.axisBottom(this.x).ticks(this.width / 80).tickSizeOuter(0);
+    this.xAxis = d3.axisBottom(this.x).ticks(xAxisTicksCount);
     this.yAxis = d3.axisLeft(this.y);
 
     this.svg.append('g').attr('class', 'xAxis').attr('transform', `translate(0, ${this.height - this.margin.bottom})`).call(this.xAxis);
@@ -116,19 +178,25 @@ export class DotLineChartComponent implements OnInit, OnChanges {
 
   private showTip(): void {
     const formatTime = d3.timeFormat('%m/%d/%y');
+
     const moved = () => {
       d3.event.preventDefault();
       const mouse = d3.mouse(d3.event.target);
-      const xm: any = this.x.invert(mouse[0]);
+      // const xm: any = this.x.invert(mouse[0]);
+      const position = d3.bisectLeft(this.xRangeArray, mouse[0]);
+      const xm = 0;
       const ym = this.y.invert(mouse[1]);
-      const i1 = d3.bisectLeft(this.data.dates, xm , 1);
+      const i1 = position; // d3.bisectLeft(this.xRangeArray, xm , 1);
       const i0 = i1 - 1;
-      const i = xm - this.data.dates[0] > this.data.dates[i1] - xm ? i1 : i0;
+      let i = xm - this.data.dates[0] > this.data.dates[i1] - xm ? i1 : i0;
+      i = i1;
       const s: any = least(this.data.series, (r: any) => Math.abs(r.values[i] - ym));
-      d3.selectAll('.line path').attr('stroke', v => v === s ? null : '#ddd').filter(v => v === s).raise();
-      dot.attr('transform', `translate(${this.x(this.data.dates[i])}, ${this.y(s.values[i])})`);
-      dot.select('.tooltip-date').text(formatTime(this.data.dates[i]));
-      dot.select('.tooltip-likes').text(s.values[i]);
+      if (s) {
+        d3.selectAll('.line path').attr('stroke', v => v === s ? null : '#ddd').filter(v => v === s).raise();
+        dot.attr('transform', `translate(${this.x(this.data.dates[i])}, ${this.y(s.values[i])})`);
+        dot.select('.tooltip-date').text(formatTime(this.data.dates[i]));
+        dot.select('.tooltip-likes').text(s.values[i]);
+      }
     };
     const entered = () => {
       d3.selectAll('.line path').style('mix-blend-mode', null).attr('stroke', '#ddd');
