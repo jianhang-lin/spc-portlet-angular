2baff58206fc39e5fcfdbe58be6fb08710e086f9
Grant Lin <grant.lin@42-q.com>
2020-10-30 13:54:40 +0800

    refactor(function): remove import HomeModule code

--- src/app/function/function.module.ts
+++ src/app/function/function.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FunctionListComponent } from './function-list/function-list.component';
 import { SharedModule } from '../shared/shared.module';
-import { HomeModule } from '../home/home.module';
 import { FunctionRoutingModule } from './function-routing.module';
 import { FunctionStepGridListComponent } from './function-step-grid-list/function-step-grid-list.component';
 import { FunctionStepGridComponent } from './function-step-grid/function-step-grid.component';
@@ -10,7 +9,6 @@ import { FunctionStepGridComponent } from './function-step-grid/function-step-gr
   declarations: [FunctionListComponent, FunctionStepGridListComponent, FunctionStepGridComponent],
   imports: [
     SharedModule,
-    HomeModule,
     FunctionRoutingModule,
   ]
 })
