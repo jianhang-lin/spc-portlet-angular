ebcb0dea25853c1d4a12574a68480fef3129e97c
Grant Lin <grant.lin@42-q.com>
2020-11-06 14:33:48 +0800

    feat(actions): update chart-bar-options action

--- src/app/actions/chart-bar-options.action.ts
+++ src/app/actions/chart-bar-options.action.ts
@@ -3,6 +3,8 @@ import { Action } from '@ngrx/store';
 
 export const ActionTypes = {
   SELECT_CHART_TYPE: type('[ChartBarOptions] Select Chart Type'),
+  SELECT_CHART_TYPE_SUCCESS: type('[ChartBarOptions] Select Chart Type Success'),
+  SELECT_CHART_TYPE_FAIL: type('[ChartBarOptions] Select Chart Type Fail'),
   SELECT_DATE_TIME_RANGE: type('[ChartBarOptions] Select Date Time Range'),
   SELECT_REVISION: type('[ChartBarOptions] Select Revision'),
   SELECT_RETRIEVE: type('[ChartBarOptions] Select Retrieve'),
@@ -10,6 +12,16 @@ export const ActionTypes = {
 
 export class SelectChartTypeAction implements Action {
   type = ActionTypes.SELECT_CHART_TYPE;
+  constructor(public payload: string) {}
+}
+
+export class SelectChartTypeSuccessAction implements Action {
+  type = ActionTypes.SELECT_CHART_TYPE_SUCCESS;
+  constructor(public payload: string) {}
+}
+
+export class SelectChartTypeFailAction implements Action {
+  type = ActionTypes.SELECT_CHART_TYPE_FAIL;
   constructor(public payload: string) {}
 }
 
@@ -28,7 +40,7 @@ export class SelectRetrieveAction implements Action {
   constructor(public payload: string) {}
 }
 
-export type Actions = SelectChartTypeAction |
+export type Actions = SelectChartTypeAction | SelectChartTypeSuccessAction | SelectChartTypeFailAction |
   SelectDateTimeRangeAction |
   SelectRevisionAction |
   SelectRetrieveAction;
