3c9d1f2d5945eab6e2b14dae49bed8deda3ed596
Grant Lin <grant.lin@42-q.com>
2020-11-06 14:35:38 +0800

    feat(chart-bar-options): implement SELECT_CHART_TYPE_SUCCESS reducer

--- src/app/reducers/chart-bar-options.reducer.ts
+++ src/app/reducers/chart-bar-options.reducer.ts
@@ -12,22 +12,30 @@ export const initialState: State = {
 
 export function reducer(state = initialState, action: chartBarOptionsActions.Actions ): State {
   switch (action.type) {
-    case chartBarOptionsActions.ActionTypes.SELECT_CHART_TYPE: {
-      return Object.assign({}, state, {
-        chartType: action.payload
-      });
+    case chartBarOptionsActions.ActionTypes.SELECT_CHART_TYPE_SUCCESS: {
+      return {
+        chartBarOptions: new ChartBarOptionsModelBuilder().create(action.payload,
+          state.chartBarOptions.startTime,
+          state.chartBarOptions.endTime,
+          state.chartBarOptions.dateTimeRange,
+          state.chartBarOptions.revision,
+          state.chartBarOptions.retrieve)
+      };
     }
     case chartBarOptionsActions.ActionTypes.SELECT_DATE_TIME_RANGE: {
+      console.log('reducer:' + action.payload);
       return Object.assign({}, state, {
         dateTimeRange: action.payload
       });
     }
     case chartBarOptionsActions.ActionTypes.SELECT_REVISION: {
+      console.log('reducer:' + action.payload);
       return Object.assign({}, state, {
         revision: action.payload
       });
     }
     case chartBarOptionsActions.ActionTypes.SELECT_RETRIEVE: {
+      console.log('reducer:' + action.payload);
       return Object.assign({}, state, {
         retrieve: action.payload
       });
