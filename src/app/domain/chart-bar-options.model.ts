dbe94bebf26929bc32e47d6641f760cfddc811a4
Grant Lin <grant.lin@42-q.com>
2020-11-06 14:31:42 +0800

    feat(chart-bar-options): add create method

--- src/app/domain/chart-bar-options.model.ts
+++ src/app/domain/chart-bar-options.model.ts
@@ -22,4 +22,16 @@ export class ChartBarOptionsModelBuilder {
       retrieve: false,
     };
   }
+
+  public create(chartType: string, endTime: Date, startTime: Date, dateTimeRange: string,
+                revision: number, retrieve: boolean): ChartBarOptionsModel {
+    return {
+      chartType,
+      endTime,
+      startTime,
+      dateTimeRange,
+      revision,
+      retrieve,
+    };
+  }
 }
