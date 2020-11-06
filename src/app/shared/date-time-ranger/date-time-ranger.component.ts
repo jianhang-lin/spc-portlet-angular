1b2cf5526f2a7a4972567d6adc11a5704601c832
Grant Lin <grant.lin@42-q.com>
2020-11-06 16:02:44 +0800

    feat(shared): implement date-time-ranger component

--- src/app/shared/date-time-ranger/date-time-ranger.component.ts
+++ src/app/shared/date-time-ranger/date-time-ranger.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { C_CHART, CPK_PPK_CHART, P_CHART } from '../../utils/const.util';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatSelectChange } from '@angular/material/select';
 
 @Component({
   selector: 'app-date-time-ranger',
@@ -8,26 +8,13 @@ import { C_CHART, CPK_PPK_CHART, P_CHART } from '../../utils/const.util';
 })
 export class DateTimeRangerComponent implements OnInit {
 
-  @Input() chartType: string;
-  hidden: boolean;
+  @Output() selectDateTimeRangeEvent = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.chartType);
-    switch (this.chartType) {
-      case C_CHART:
-        this.hidden = true;
-        break;
-      case P_CHART:
-        this.hidden = false;
-        break;
-      case CPK_PPK_CHART:
-        this.hidden = false;
-        break;
-      default:
-        this.hidden = true;
-        break;
-    }
   }
 
+  onSelectedDateTimeRangeChange($event: MatSelectChange) {
+    this.selectDateTimeRangeEvent.emit($event.source.value);
+  }
 }
