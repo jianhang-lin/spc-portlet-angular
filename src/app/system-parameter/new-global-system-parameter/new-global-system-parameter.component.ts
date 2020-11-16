a25ce8513ff9befb11e50981cab1fbfabcbd8fbb
Grant Lin <grant.lin@42-q.com>
2020-11-16 15:28:50 +0800

    feat(system-parameter): implement new-global_system_parameter component

--- src/app/system-parameter/new-global-system-parameter/new-global-system-parameter.component.ts
+++ src/app/system-parameter/new-global-system-parameter/new-global-system-parameter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-new-global-system-parameter',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewGlobalSystemParameterComponent implements OnInit {
 
-  constructor() { }
+  title = '';
+  form: FormGroup;
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      intervalOfCollectingData: ['', Validators.compose([Validators.required, Validators.min(30)])],
+      intervalOfDeletingLog: ['', Validators.compose([Validators.required, Validators.min(1)])],
+      timeOfLogSaved: ['', Validators.required],
+      intervalBetweenLockReviews: ['', Validators.required],
+      displayFamilyIDInChart: [''],
+      startTimeOfDataCollection: ['', Validators.required],
+      hideMonitorsWithNoData: ['', Validators.required],
+      hideOnTV: ['', Validators.required],
+      enablePreviousSpcHold: [''],
+      emailTitle: [''],
+      mesrUrlTemplate: [''],
+    });
+    this.title = 'SPC System Parameter';
   }
 
+
+  onSubmit({value, valid}, en: Event) {
+    en.preventDefault();
+    if (!valid) {
+      return;
+    }
+    console.log(JSON.stringify(value));
+  }
 }
